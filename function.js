29c3f87e9fc2152d4a6698624c929aeb0b034d27
byoukinn <1209418977@qq.com>
2018-10-21 23:37:56 +0800

    add floating red text

--- function.js
+++ function.js
@@ -1,156 +1,224 @@
-$(function(){
-	var log = console.log.bind(console);
-    // select a
-	var $a = $('.post-content a')
-	$a.mouseover(function (e) {
-	    this.__mouseover = true;
-	    this.__t = this.title;
-		this.title = '';
-		let template = `<div class="tip">${this.__t}</div>`;
-		$('body').append(template);
-		this.tip = $('.tip')
+$(function () {
+    var log = console.log.bind(console);
+    var $a = $('.post-content a');
+    $a.mouseover(function (e) {
+        // 初始化数据，然后显示额外的提示
+        this.__mouseover = true;
+        this.__t = this.title;
+        this.title = '';
+        let template = `<div class="tip">${this.__t}</div>`;
+        $('body').append(template);
+        this.tip = $('.tip');
         this.tip.css({
-            'display':'none',
+            'display': 'none',
         });
         this.tip.slideDown('fast')
     }).mousemove(function (e) {
+        // 鼠标移动的时候跟着走
         if (!this.__mouseover)
             return;
         let offsetX = 35;
         this.tip.css('top', e.pageY);
         this.tip.css('left', e.pageX + offsetX);
     }).mouseout(function () {
+        // 鼠标退出了，把额外的提示给去掉
         this.__mouseover = false;
-		this.title = this.__t
+        this.title = this.__t;
         this.tip.slideUp('fast', function () {
             this.remove();
         })
     })
 
-    var $img = $('.post-content img')
+    var $img = $('.post-content img');
     $img.mouseover(function (e) {
-        this.mouse_in = true;
-        this.__a = this.alt
-        this.__t = this.title
-        this.alt = ''
-        this.title = ''
-        $(this).css('cursor', 'zoom-in')
+        // 初始化数据，显示出图片型的额外提示，会有过渡效果
+        this.__mousedown = true;
+        this.__a = this.alt;
+        this.__t = this.title;
+        this.alt = '';
+        this.title = '';
+        $(this).css('cursor', 'zoom-in');
         let template = `<div class='tip imgtip'><img id='tipimg' src="${this.src}" witdh='100%' height='100%'></div>`;
         $('body').append(template);
-        this.tip = $('.tip')
+        this.tip = $('.tip');
         this.tip.css('display', 'none');
-        $(this).animate({opacity: '0.1'}, 'fast')
+        $(this).stop();
+        $(this).animate({opacity: '0.1'}, 'fast');
         this.tip.fadeIn('fast')
 
     }).mousemove(function (e) {
-        if (!this.mouse_in)
+        // 鼠标移动时，图片型额外提示跟着鼠标走
+        if (!this.__mousedown)
             return;
         var gap = 35
-        var isHorizon = this.naturalWidth > window.innerWidth / 3 ;
-        let offsetX = isHorizon ?  -this.naturalWidth / 2 : gap ;
-        let offsetY = isHorizon ?  gap : -this.naturalHeight / 2;
+        var isHorizon = this.naturalWidth > window.innerWidth / 3;
+        let offsetX = isHorizon ? -this.naturalWidth / 2 : gap;
+        let offsetY = isHorizon ? gap : -this.naturalHeight / 2;
         this.tip.css({
             'left': e.pageX + offsetX,
             'top': e.pageY + offsetY
         });
-        // log(tip_object.css('top'), tip_object.css('left'));
     }).mouseout(function () {
-        this.mouse_in = false;
-        this.alt = this.__a
-        this.title = this.__t
-        $(this).animate({opacity: '1'}, 'fast')
+        // 鼠标离开了，结束事件，去掉额外提示
+        this.__mousedown = false;
+        this.alt = this.__a;
+        this.title = this.__t;
+        $(this).stop();
+        $(this).animate({opacity: '1'}, 'fast');
         this.tip.fadeOut('fast', function () {
             this.remove();
         })
     })
-    
 
 
-    // blackbox
-    // mousedown 有一个参数 event 会传递到他的回调函数 f 去
-    // f 拿到了 event ，因为js是一个动态语言，可以随便叫这个event任意名字，所以你可以写成 e
-    // e => 一个鼠标事件源元素
-
-    // TODO: 
+    // sharkarstain
+    // 功能：
     // 1. 点击透明黑色区域才关闭
     // 2. 滚轮向上放大图片
     // 3. 滚轮向下缩小图片
     // 4. 按住图片拖动图片大小
     // 5. 导航图
     $img.mousedown(function (e) {
-        e.preventDefault()
+        // 鼠标点中了图片，进入图片展示模式
+        e.stopPropagation();
+        e.preventDefault();
         if (e.button != 0)
-            return 
-        var template = `<div class='sharkarstain'><img class='inbox_image' src="${ this.src }"></div>`;
+            return;
 
+        
+
+        var template = `<div class='sharkarstain'><img class='inbox_image' src="${ this.src }"></div>`;
         $('body').append(template);
-        var $div = $('.sharkarstain')
-        var $image = $('.inbox_image')
-        $div.fadeIn('slow').mousedown(function(e) {
-            if ($div.__mouseover_img || e.button == 2)
-                return
+        var $div = $('.sharkarstain');
+        var $image = $('.inbox_image');
+        // 具有过渡效果的显示和消失，只有在鼠标右键整个画布，鼠标左键了背景才起作用
+        $div.fadeIn('slow').mousedown(function (e) {
             $(this).fadeOut('slow', function () {
                 $(this).remove();
             })
-        }).mouseup(function() {
-            $image.mouse_in = false
-        }).bind('mousewheel', function(e) {
-            e.preventDefault()
-            var delta = e.originalEvent.deltaY / 100 
-            var percent = delta < 0 ? 1.1 : 0.9
-            var w = $image.width()
-            var h = $image.height()
-            $image.width(w * percent)
-            $image.height(h * percent)
-        })
+        }).mouseup(function () {
+            $image.__mousedown = false
+        }).bind('mousewheel', function (e) {
+            // 带有过渡效果的鼠标滚轮缩小放大
+            e.preventDefault();
+            var delta = e.originalEvent.deltaY / 100;
+            var percent = delta < 0 ? 1.4 : 0.6;
+            var w = $image.width();
+            var h = $image.height();
+            $image.stop();
+            $image.animate({
+                width: w * percent,
+                height: h * percent,
+            }, "fast")
+        });
 
-        $image.mousedown(function(e) {
-            e.preventDefault()
-            $image.mouse_in = true
-            var h2 = $(this).height() / 2
-            var w2 = $(this).width() / 2
-            this.offsetY =  h2 - e.offsetY
-            this.offsetX =  w2 - e.offsetX
-        }).mouseover(function() {
-            $div.__mouseover_img = true
-        }).mouseout(function() {
-            $div.__mouseover_img = false
-        }).mousemove(function(e) {
-            if (!$image.mouse_in)
-                return
+        $image.mousedown(function (e) {
+            e.stopPropagation();
+            e.preventDefault();
+            $image.__mousedown = true;
+            var h2 = $(this).height() / 2;
+            var w2 = $(this).width() / 2;
+            this.offsetY = h2 - e.offsetY;
+            this.offsetX = w2 - e.offsetX
+        }).mouseover(function () {
+            $image.__mouseover = true
+        }).mouseout(function () {
+            $image.__mouseover = false
+        }).mousemove(function (e) {
+            if (!$image.__mousedown)
+                return;
             $(this).css({
-                'top' : e.clientY + this.offsetY, // 要图片的点跟着鼠标拖着的点走
+                'top': e.clientY + this.offsetY, // 要图片的点跟着鼠标拖着的点走
                 'left': e.clientX + this.offsetX,
             })
         })
-    })
-    
+    });
 
+    // 鼠标左键后漂浮字体
+    class FloatingText {
+        constructor() {
+            this.el = ['富强民主', '文明和谐', '自由平等', '公正法制', '爱国敬业', '诚信友善',];
+            this.current = 0;
+            this.count = 0;
+            this.offset = 120;
+            this.$container = null;
+            FloatingText.__instance = null;
+            this.initial()
+        }
+
+        static getInstance() {
+            if (FloatingText.__instance == null)
+                FloatingText.__instance = new FloatingText();
+            return FloatingText.__instance;
+        }
+
+        initial() {
+            if (FloatingText.__instance != null)
+                return;
+            this.$container = $('body');
+        }
+
+        getNextItem() {
+            var ret = this.el[this.current];
+            this.current = (this.current + 1) % this.el.length;
+            return ret;
+        }
+
+        showText(event) {
+            this.count += 1;
+            var id = `floating_text${this.count}`;
+            var template = `<p id="${id}" class="floating_text">${this.getNextItem()}</p>`;
+            this.$container.append(template);
+            var self = this;
+            $('#'+id).css({
+                'top': event.pageY - self.offset / 2,
+                'left': event.pageX, // 样式表中定义他已经居中
+            }).animate({
+                'top': event.pageY - self.offset,
+                'opacity': 0,
+            }, 1000, function () {
+                this.remove()
+            })
+        }
+    }
 
     // 屏蔽默认右键菜单
     // fixed:
     // 1. 导航栏菜单
     // 2. 整个目录的显示
     !function replaceContextMenu() {
-        var indexmenu = document.getElementsByClassName('index-menu')[0]
-        if (!indexmenu) 
+        var indexmenu = document.getElementsByClassName('index-menu')[0];
+        if (!indexmenu)
             return
         var html = indexmenu.outerHTML;
         var template = `<div class="context-menu"><b>目录</b>${ html }</div>`;
         $('body').append(template);
         var $ctxmenu = $('.context-menu');
-        document.oncontextmenu = function(e) {
+        document.oncontextmenu = function (e) {
             e.preventDefault();
-            $ctxmenu.css({
-                'top': e.pageY,
-                'left': e.pageX,
-            });
-            $ctxmenu.fadeIn('fast');
-        }
-        window.onclick = function(e) {
-            $ctxmenu.fadeOut('fast');
-        }
-    }()
-    
+        };
+        // 加入了浮动文字
+        var floatingText = FloatingText.getInstance();
+        $(window).mousedown(function (e) {
+            if (e.button == 2) {
+                $ctxmenu.css({
+                    'top': e.pageY,
+                    'left': e.pageX,
+                });
+                $ctxmenu.fadeIn('fast');
+                $ctxmenu.__exist = true;
+            } else if (e.button == 0) {
+                $ctxmenu.fadeOut('fast');
+                if ($ctxmenu.__exist) {
+                    $ctxmenu.__exist = false;
+                    return;
+                }
+                floatingText.showText(e);
 
-})
+            }
+        });
+
+    }();
+
+
+});
